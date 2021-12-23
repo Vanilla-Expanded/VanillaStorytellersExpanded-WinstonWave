bfc73c74b492d78e5749e2ee9639896b5eb120dc
KylianB <kylian.balde@gmail.com>
2021-12-23 18:34:03 +0100

    Reward sender start

---
+++ 1.3/Source/VSEWW/VSEWW/DebugOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VSEWW
+{
+    public static class DebugOptions
+    {
+        [DebugAction("VES Winston Wave", "Rewards test", false, false, actionType = DebugActionType.Action, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        public static void RewardTest()
+        {
+            List<DebugMenuOption> debugMenuOptionList = new List<DebugMenuOption>();
+            List<RewardDef> rewards = DefDatabase<RewardDef>.AllDefsListForReading;
+
+            foreach (var r in rewards)
+            {
+                debugMenuOptionList.Add(new DebugMenuOption(r.defName.Remove(0, 6), DebugMenuOptionMode.Action, () => RewardCreator.SendReward(r, Find.CurrentMap)));
+            }
+            Find.WindowStack.Add(new Dialog_DebugOptionListLister(debugMenuOptionList));
+        }
+    }
+}
---
+++ 1.3/Source/VSEWW/VSEWW/RewardCreator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace VSEWW
+{
+    public static class RewardCreator
+    {
+        public static void SendReward(RewardDef reward, Map map)
+        {
+            if (reward is IncidentRewardDef incidentRewardDef)
+            {
+                Find.Storyteller.incidentQueue.Add(incidentRewardDef.incidentDef, Find.TickManager.TicksGame, new IncidentParms
+                {
+                    target = map
+                });
+            }
+            else
+            {
+                List<Thing> thingList = CreateThingListFromRewardDef(reward);
+                IntVec3 intVec3 = DropCellFinder.TryFindSafeLandingSpotCloseToColony(map, ThingDefOf.DropPodIncoming.Size, map.ParentFaction);
+
+                Log.Message($"Sending {thingList.Count} things to {intVec3}");
+                DropPodUtility.DropThingsNear(intVec3, map, thingList, leaveSlag: true);
+            }
+        }
+
+        private static List<Thing> CreateThingListFromRewardDef(RewardDef reward)
+        {
+            List<Thing> things = new List<Thing>();
+
+            if (reward is BlockRewardDef) things.AddRange(FromBlockRewardDef(reward));
+
+            return things;
+        }
+
+        private static List<Thing> FromBlockRewardDef(RewardDef reward)
+        {
+            List<Thing> things = new List<Thing>();
+            BlockRewardDef blockRewardDef = reward as BlockRewardDef;
+
+            ThingDef blockDef = DefDatabase<ThingDef>.AllDefsListForReading.FindAll(t => t.thingCategories != null && t.thingCategories.Contains(ThingCategoryDefOf.StoneBlocks)).RandomElement();
+            int countLeft = blockRewardDef.count;
+            while (countLeft > 0)
+            {
+                Thing block = ThingMaker.MakeThing(blockDef);
+                int stack = Math.Min(countLeft, blockDef.stackLimit);
+                block.stackCount = stack;
+                countLeft -= stack;
+                things.Add(block);
+            }
+
+            return things;
+        }
+    }
+}
