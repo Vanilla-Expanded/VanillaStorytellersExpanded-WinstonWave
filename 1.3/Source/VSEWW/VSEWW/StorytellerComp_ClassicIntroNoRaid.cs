f76247d368c4d6003a6cd0e23a1311cd1978ffe3
KylianB <kylian.balde@gmail.com>
2021-12-23 09:19:56 +0100

    StorytellerDef and no raid intro

---
+++ 1.3/Source/VSEWW/VSEWW/StorytellerComp_ClassicIntroNoRaid.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace VSEWW
+{
+    public class StorytellerCompProperties_ClassicIntroNoRaid : StorytellerCompProperties
+    {
+        public StorytellerCompProperties_ClassicIntroNoRaid() => this.compClass = typeof(StorytellerComp_ClassicIntroNoRaid);
+    }
+
+    public class StorytellerComp_ClassicIntroNoRaid : StorytellerComp
+    {
+        protected int IntervalsPassed => Find.TickManager.TicksGame / 1000;
+
+        public override IEnumerable<FiringIncident> MakeIntervalIncidents(IIncidentTarget target)
+        {
+            StorytellerComp_ClassicIntroNoRaid compCINR = this;
+            if (target == Find.Maps.Find(x => x.IsPlayerHome))
+            {
+                if (compCINR.IntervalsPassed == 150)
+                {
+                    if (IncidentDefOf.VisitorGroup.TargetAllowed(target))
+                    {
+                        yield return new FiringIncident(IncidentDefOf.VisitorGroup, compCINR)
+                        {
+                            parms = {
+                                target = target,
+                                points = Rand.Range(40, 100)
+                            }
+                        };
+                    }
+                }
+                if (compCINR.IntervalsPassed == 204 && DefDatabase<IncidentDef>.AllDefs.Where(def => def.TargetAllowed(target) && def.category == (Find.Storyteller.difficulty.allowIntroThreats ? IncidentCategoryDefOf.ThreatSmall : IncidentCategoryDefOf.Misc))
+                        .TryRandomElementByWeight(compCINR.IncidentChanceFinal, out IncidentDef result))
+                {
+                    yield return new FiringIncident(result, compCINR)
+                    {
+                        parms = StorytellerUtility.DefaultParmsNow(result.category, target)
+                    };
+                }
+                if (compCINR.IntervalsPassed == 264 && DefDatabase<IncidentDef>.AllDefs.Where(def => def.TargetAllowed(target) && def.category == IncidentCategoryDefOf.Misc)
+                    .TryRandomElementByWeight(compCINR.IncidentChanceFinal, out IncidentDef result1))
+                {
+                    yield return new FiringIncident(result1, compCINR)
+                    {
+                        parms = StorytellerUtility.DefaultParmsNow(result1.category, target)
+                    };
+                }
+            }
+        }
+    }
+}
