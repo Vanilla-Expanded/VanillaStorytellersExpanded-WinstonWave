9bf8d6962211c978833fc282b60861f680c465d5
KylianB <kylian.balde@gmail.com>
2022-01-27 11:04:43 +0100

    Reward rebalance

--- 1.3/Source/VSEWW/VSEWW/RewardCategory.cs
+++ 1.3/Source/VSEWW/VSEWW/RewardCategory.cs
@@ -26,86 +26,85 @@ namespace VSEWW
 
             if (waveN <= 5)
             {
-                com[RewardCategory.Poor] = 100;
-                com[RewardCategory.Normal] = 3;
+                com[RewardCategory.Poor] = 90;
+                com[RewardCategory.Normal] = 8;
                 com[RewardCategory.Good] = 2;
-                com[RewardCategory.Excellent] = 1;
                 return com;
             }
             if (waveN <= 10)
             {
-                com[RewardCategory.Poor] = 100;
-                com[RewardCategory.Normal] = 6;
+                com[RewardCategory.Poor] = 80;
+                com[RewardCategory.Normal] = 12;
                 com[RewardCategory.Good] = 4;
-                com[RewardCategory.Excellent] = 2;
+                com[RewardCategory.Excellent] = 3;
                 com[RewardCategory.Legendary] = 1;
                 return com;
             }
             if (waveN <= 15)
             {
-                com[RewardCategory.Poor] = 100;
-                com[RewardCategory.Normal] = 16;
+                com[RewardCategory.Poor] = 50;
+                com[RewardCategory.Normal] = 35;
                 com[RewardCategory.Good] = 10;
-                com[RewardCategory.Excellent] = 5;
+                com[RewardCategory.Excellent] = 3;
                 com[RewardCategory.Legendary] = 2;
                 return com;
             }
             if (waveN <= 20)
             {
-                com[RewardCategory.Poor] = 100;
-                com[RewardCategory.Normal] = 38;
-                com[RewardCategory.Good] = 25;
-                com[RewardCategory.Excellent] = 10;
-                com[RewardCategory.Legendary] = 4;
+                com[RewardCategory.Poor] = 20;
+                com[RewardCategory.Normal] = 50;
+                com[RewardCategory.Good] = 20;
+                com[RewardCategory.Excellent] = 7;
+                com[RewardCategory.Legendary] = 3;
                 return com;
             }
             if (waveN <= 25)
             {
-                com[RewardCategory.Poor] = 50;
-                com[RewardCategory.Normal] = 100;
-                com[RewardCategory.Good] = 38;
-                com[RewardCategory.Excellent] = 25;
-                com[RewardCategory.Legendary] = 10;
+                com[RewardCategory.Poor] = 10;
+                com[RewardCategory.Normal] = 20;
+                com[RewardCategory.Good] = 30;
+                com[RewardCategory.Excellent] = 6;
+                com[RewardCategory.Legendary] = 4;
                 return com;
             }
             if (waveN <= 30)
             {
-                com[RewardCategory.Poor] = 38;
-                com[RewardCategory.Normal] = 100;
-                com[RewardCategory.Good] = 56;
-                com[RewardCategory.Excellent] = 22;
-                com[RewardCategory.Legendary] = 14;
+                com[RewardCategory.Poor] = 5;
+                com[RewardCategory.Normal] = 30;
+                com[RewardCategory.Good] = 50;
+                com[RewardCategory.Excellent] = 10;
+                com[RewardCategory.Legendary] = 5;
                 return com;
             }
             if (waveN <= 40)
             {
-                com[RewardCategory.Poor] = 12;
-                com[RewardCategory.Normal] = 100;
-                com[RewardCategory.Good] = 100;
-                com[RewardCategory.Excellent] = 30;
-                com[RewardCategory.Legendary] = 20;
+                com[RewardCategory.Poor] = 5;
+                com[RewardCategory.Normal] = 20;
+                com[RewardCategory.Good] = 50;
+                com[RewardCategory.Excellent] = 20;
+                com[RewardCategory.Legendary] = 5;
                 return com;
             }
             if (waveN <= 50)
             {
-                com[RewardCategory.Normal] = 50;
-                com[RewardCategory.Good] = 100;
-                com[RewardCategory.Excellent] = 50;
-                com[RewardCategory.Legendary] = 20;
+                com[RewardCategory.Normal] = 20;
+                com[RewardCategory.Good] = 30;
+                com[RewardCategory.Excellent] = 40;
+                com[RewardCategory.Legendary] = 10;
                 return com;
             }
             if (waveN <= 60)
             {
-                com[RewardCategory.Normal] = 25;
-                com[RewardCategory.Good] = 100;
-                com[RewardCategory.Excellent] = 100;
-                com[RewardCategory.Legendary] = 20;
+                com[RewardCategory.Normal] = 15;
+                com[RewardCategory.Good] = 25;
+                com[RewardCategory.Excellent] = 45;
+                com[RewardCategory.Legendary] = 15;
                 return com;
             }
 
-            com[RewardCategory.Good] = 100;
-            com[RewardCategory.Excellent] = 100;
-            com[RewardCategory.Legendary] = 50;
+            com[RewardCategory.Good] = 25;
+            com[RewardCategory.Excellent] = 50;
+            com[RewardCategory.Legendary] = 25;
             return com;
         }
     }
